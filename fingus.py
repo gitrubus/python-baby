540b0e04289ebfc8093ef441b713795466daebd7
gitrubus <93807053+gitrubus@users.noreply.github.com>
2024-10-19 08:37:13 -0600

    Create fingus.py
    eeeeeeeeeeeeeeeeeeeeeeeee

---
+++ fingus.py
@@ -0,0 +1,11 @@
+print("hello world!")
+print("welcome to my show about python!")
+print("in this game show we will be revailing if chickens cluck so lets get start")
+print("question 1")
+
+x = input("can you fly?: ")
+if x == "yes!":
+  print("thats great news")
+  print("heres your free point")
+  points = 1
+print("your points are", points)_
