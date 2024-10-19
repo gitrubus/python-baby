692374d8b5b0f805de0b3c3c823ccd850466e986
gitrubus <93807053+gitrubus@users.noreply.github.com>
2024-10-19 08:53:56 -0600

    Update fingus.py

--- fingus.py
+++ fingus.py
@@ -2,10 +2,35 @@ print("hello world!")
 print("welcome to my show about python!")
 print("in this game show we will be revailing if chickens cluck so lets get start")
 print("question 1")
+points = 0
 
 x = input("can you fly?: ")
-if x == "yes!":
+if x == "yes":
   print("thats great news")
   print("heres your free point")
-  points = 1
+  points = points + 1
 print("your points are", points)
+
+e = input("is python easy?: ")
+if e == "yes":
+  print("thats awesome!")
+  points = points + 1
+else:
+  print("incorrect")
+  print("💀💀💀💀💀")
+
+df = input(:)
+if df == what?:
+  print("correct")
+else:
+  no way
+print("the end!")
+while true:
+  gh = 90 + 8
+  oip = 70 % 4
+  Jazmine = 5
+
+  
+  print(6 + 9)
+  print(7 + oip)
+  print(gh + jazmine)
